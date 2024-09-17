a964c382c3631560a4961a917ebc4a8038b14a3c
Steven Coltman <steven.coltman@hess-natur.de>
2024-09-17 11:26:09 +0200

    switching to foodie app

--- app/blog/[slug]/page.js
+++
@@ -1,6 +0,0 @@
-export default function Post({params}) {
-    return <main>
-        <h1>Blog post</h1>
-        <p>Slug: {params.slug}</p>
-    </main>
-}
--- app/page.js
+++ app/page.js
@@ -1,19 +1,9 @@
-import Link from "next/link";
-import Header from "@/components/header"
-import Image from "next/image";
-
 export default function Home() {
-    return (
-        <main>
-            <Header/>
-            <h1>Welcome to this NextJS Course!</h1>
-            <p>🔥 Let&apos;s get started! 🔥</p>
-            <p>
-                <Link href="/about">About</Link>
-            </p>
-            <p>
-
-                <Link href="/blog">Blog</Link></p>
-        </main>
-    );
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+    </main>
+  );
 }
